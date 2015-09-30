80326e0c9426e20646916e246308c0eef3c75e3e
HJPark <hyeonjoopark88@gmail.com>
2015-10-01 01:08:16 +0900

    Update sketch.ino

--- ArduinoDebugger-Fixed/sketch.ino
+++ ArduinoDebugger-Fixed/sketch.ino
@@ -1,8 +1,56 @@
-void setup()
-{  
-  Serial.begin (115200);
-  Serial.println ( "Open LEDSwitch.txt project" );
+class Pinn{
+	int pinnum;
+	int delay;
+	int state;
+	
+	public:
+		void led();
+		void setDelay(int delay);
+		int getPinnum();
+		int getDelay();
+		
+		Pinn();
+		Pinn(int pinnum);
+};
+void Pinn::led(){
+	digitalWrite(pinnum,state);
+	state = !state;
 }
-void loop ()
+Pinn::Pinn(){
+	pinnum=13;
+	delay=1000;
+	state=0;
+}
+Pinn::Pinn(int pinnum){
+	this->pinnum = pinnum;
+	delay=1000;
+	state=0;
+}
+int Pinn::getPinnum(){
+	return pinnum;
+}
+void Pinn::setDelay(int delay){
+	this->delay = delay;
+}
+int Pinn::getDelay(){
+	return delay;
+}
+
+Pinn pinn(13);
+
+void setup()
 {
+  pinMode (pinn.getPinnum(),OUTPUT);
+  pinn.setDelay(1500);
+}
+
+unsigned long timeout = 0;
+
+void loop()
+{
+  if (millis() > timeout)
+  {
+  	timeout = millis() + pinn.getDelay();
+  	pinn.led();
+  }
 }
